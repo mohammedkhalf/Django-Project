448b91f533cef5993c31aaf7838565c775c4d9fc
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-02-10 22:30:27 +0200

    create view with Url

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index (request):
-    return HttpResponse('Hello Test World')
+    return HttpResponse('Index Page')
+
+def about(request):
+    return HttpResponse('about page')
