335b959e7d7e4d14429f2ad423445d69191d6b85
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-02-11 00:51:07 +0200

    create templates with render template html

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index (request):
-    return HttpResponse('Index Page')
+    return render(request,'pages/index.html')
 
 def about(request):
     return HttpResponse('about page')
