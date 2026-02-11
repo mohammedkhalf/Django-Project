2aed3756b45d7fa8d2b61a208b307c6bb299cf7b
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-02-12 00:27:11 +0200

    filter DTL - Django template language

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index (request):
-    x = {'name':'ahmed' , 'age':'45 years'}
+    x = {'name':'ali mohamed' , 'age':'45 years'}
     return render(request,'pages/index.html',x)
 
 def about(request):
