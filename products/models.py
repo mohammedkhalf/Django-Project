169ffc4a17c2a56054460a4337fe7d51ca2eb526
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-02-14 19:55:27 +0200

    add fields with migration

--- products/models.py
+++ products/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 class Product (models.Model):
     
-    name = models.CharField(max_length=50)
-    content = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to='photos/%y/%m/%d')
+    x = [
+        ('phone','phone'),
+        ('computer', 'computer'),
+    ]
+    
+    name = models.CharField(max_length=50, default='name',verbose_name='title')
+    content = models.TextField(null=True,blank=True, verbose_name='description')
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=10.0)
+    image = models.ImageField(upload_to='photos/%y/%m/%d',verbose_name='photo')
+    category = models.CharField(max_length=50,null =True,blank=True,choices=x)
     active = models.BooleanField(default=True)
     
     def __str__(self):
@@ -16,4 +22,3 @@ class Product (models.Model):
     class Meta:   # meta class for some apply some feature like ordering , naming and other feature
         ordering = ['-price'] # minus for desc and without - is asc
 
-    
