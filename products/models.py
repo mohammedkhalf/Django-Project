3f2cc3c499d0ff4ce23b252ddda58d2501c6f6bd
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-02-14 17:37:02 +0200

    meta class feature

--- products/models.py
+++ products/models.py
@@ -9,3 +9,11 @@ class Product (models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='photos/%y/%m/%d')
     active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:   # meta class for some apply some feature like ordering , naming and other feature
+        ordering = ['-price'] # minus for desc and without - is asc
+
+    
