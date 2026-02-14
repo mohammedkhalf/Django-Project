d8df0f87e8fa813bd757027ce1569b739614e151
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-02-15 00:12:30 +0200

    one to one relationship

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Female, Male
 
 # Register your models here.
+admin.site.register(Female)
+admin.site.register(Male)
--- pages/models.py
+++ pages/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 class Female (models.Model):
     name_female = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return self.name_female
     
 class Male (models.Model):
     name_male = models.CharField(max_length=50, null=True)
+    def __str__(self):
+            return self.name_male
     girls = models.OneToOneField(Female, on_delete=models.CASCADE)
-    
+     
     
