492f8703365924dee19864b89677206d1080c151
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-02-15 22:32:18 +0200

    query set by get and filter and all

--- products/views.py
+++ products/views.py
@@ -3,7 +3,10 @@ from .models import Product
 
 # create index products page
 def index(request):
-    return render(request,'products/index.html',{'pro':Product.objects.all()})
+    # print("data", Product.objects.get(id=2))
+    # breakpoint()
+    x = {'pro':Product.objects.all().filter(price=12)}
+    return render ( request,'products/index.html',x)
 
 # create show product
 def show (request):
