48b90ecfb16085e8417d0e5ebbec51c80dc8ec0b
Mohamed Khalf <mohamed.khalf@tamara.co>
2026-02-15 02:20:35 +0200

    one to many relationship

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Female, Male
+from .models import Female, Male, User, Product
 
 # Register your models here.
 admin.site.register(Female)
 admin.site.register(Male)
+admin.site.register(User)
+admin.site.register(Product)
+
--- pages/models.py
+++ pages/models.py
@@ -11,5 +11,20 @@ class Male (models.Model):
     def __str__(self):
             return self.name_male
     girls = models.OneToOneField(Female, on_delete=models.CASCADE)
-     
+    
+    
+    
+         
+class Product (models.Model):
+    title = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return self.title    
+    
+class User (models.Model):
+    name = models.CharField(max_length=50, null=True)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    
+
     
